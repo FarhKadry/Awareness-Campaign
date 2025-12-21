c47665a5f31b3a484c13f0e27a60a15697e9517b
Farh Kadry <23-101289@students.eui.edu.eg>
2025-12-21 19:56:03 +0200

    header structure

---
+++ home.js
@@ -0,0 +1,33 @@
+let navItems = [
+    {
+        id: "Home",
+        name: "Home",
+    },
+    {
+        id: "About",
+        name: "About",
+    },
+    {
+        id: "Why",
+        name: "Why It's Fading",
+    },
+    {
+        id: "Gallery",
+        name: "Gallery",
+    },
+    {
+        id: "Action",
+        name: "Take Action",
+    }
+];
+let navUl = document.getElementById('navArray').querySelector('ul');
+
+for (let i = 0; i < navItems.length; i++) {
+    navUl.innerHTML += `
+    <li>
+        <a href="#${navItems[i].id}">
+            ${navItems[i].name}
+        </a>
+    </li>
+    `;
+}
