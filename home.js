7843e55726bd7f54939d210b81fac1220072f380
Farh Kadry <23-101289@students.eui.edu.eg>
2025-12-22 09:24:09 +0200

    scrollbar styled

--- home.js
+++ home.js
@@ -61,3 +61,26 @@ function readMore(){
         button.textContent = 'Read More';
     }
 }
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
